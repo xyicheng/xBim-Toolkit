68b3afb9fc293acd960480ef468995e6adc8fa07
SteveLockley_cp <9623b022d0bb1e357feec713e5d0abd194558924hm7A5WAR>
2012-05-11 14:54:54 +0000

    Branched from $/xBIM/XbimFramework/Rel/RC1
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS13]$/xBIM/XbimFramework/Dev/Steve;C14189

---
+++ Xbim.Ifc.Extensions/NamedUnitExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xbim.Ifc.MeasureResource;
+
+namespace Xbim.Ifc.Extensions
+{
+    public static class NamedUnitExtensions
+    {
+        public static string Name(this IfcNamedUnit namedUnit)
+        {
+            if (namedUnit is IfcSIUnit)
+                return ((IfcSIUnit)namedUnit).Name.ToString();
+            else if(namedUnit is IfcConversionBasedUnit)
+                return ((IfcConversionBasedUnit)namedUnit).Name;
+            else if(namedUnit is IfcContextDependentUnit)
+                return ((IfcContextDependentUnit)namedUnit).Name;
+            else
+                return "";
+        }
+    }
+}
