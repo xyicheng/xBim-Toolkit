eea6c27499018eb695fd0d8678c5759bc464bb6e
andyward4p_cp <23d1e54b1a95a4b596208405dea5b98c71754f25jLApcpXs>
2012-05-30 23:02:22 +0000

    Centralised the Assembly version configuration.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS13]$/xBIM/XbimFramework/Dev/4Projects;C15172

--- Xbim.Common/Properties/AssemblyInfo.cs
+++ Xbim.Common/Properties/AssemblyInfo.cs
@@ -22,12 +22,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Xbim.Common")]
 [assembly: AssemblyDescription("Xbim Common Components")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Lockley Consulting")]
-[assembly: AssemblyProduct("Xbim.Common")]
-[assembly: AssemblyCopyright("Copyright © Lockley Consulting 2007")]
-[assembly: AssemblyTrademark("Xbim")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -38,19 +32,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("410B9307-6607-409D-BF21-9DCA5A7D3430")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build NumberOfCellsInRow
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-
-[assembly: AssemblyVersion("2.3.*")]
-[assembly: AssemblyFileVersion("2.3.1.1")] //Ifc 2x3 TC 1
 [assembly: CLSCompliant(true)]
