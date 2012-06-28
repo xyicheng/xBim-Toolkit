e97d31815ad0089808acd6e8df9b8a1ddd6601aa
SteveLockley_cp <9623b022d0bb1e357feec713e5d0abd194558924hm7A5WAR>
2012-06-28 09:54:32 +0000

    OpenCascade upgraded to 6.5.3 The bin directory has been removed and the OCC dlls now are copied to the bin output directories post build. Debug versions of the OpenCascade libraries are now no longer included This reduces the overall size of the source distributable.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS13]$/xBIM/XbimFramework/Dev/OpenCascadeUpgrade;C16714

---
+++ UpgradeOCClib.bat
@@ -0,0 +1,77 @@
+@echo off
+set src=C:\OpenCASCADE6.5.3\ros
+set target=G:\Documents\XbimCP\XbimFramework\Dev\OpenCascadeUpgrade
+echo Copying from %src% to %target%
+echo Win32 Library Release files
+pause
+mkdir %target%
+
+mkdir %target%\Xbim.ModelGeometry\OpenCascade
+mkdir %target%\Xbim.ModelGeometry\OpenCascade\Win32
+mkdir %target%\Xbim.ModelGeometry\OpenCascade\Win64
+mkdir %target%\Xbim.ModelGeometry\OpenCascade\Win32\lib 
+mkdir %target%\Xbim.ModelGeometry\OpenCascade\Win64\lib 
+mkdir %target%\Xbim.ModelGeometry\OpenCascade\Win64\bin 
+mkdir %target%\Xbim.ModelGeometry\OpenCascade\Win64\bin 
+
+
+xcopy %src %\win32\vc10\lib\TKBool.lib %target%\Xbim.ModelGeometry\OpenCascade\Win32\lib /D 
+xcopy %src %\win32\vc10\lib\TKernel.lib %target%\Xbim.ModelGeometry\OpenCascade\Win32\lib /D
+xcopy %src %\Win32\vc10\lib\TKAdvTools.lib %target%\Xbim.ModelGeometry\OpenCascade\Win32\lib /D
+xcopy %src %\win32\vc10\lib\TKMath.lib %target%\Xbim.ModelGeometry\OpenCascade\Win32\lib /D
+xcopy %src %\win32\vc10\lib\TKTopAlgo.lib %target%\Xbim.ModelGeometry\OpenCascade\Win32\lib /D
+xcopy %src %\win32\vc10\lib\TKGeomAlgo.lib %target%\Xbim.ModelGeometry\OpenCascade\Win32\lib /D
+xcopy %src %\win32\vc10\lib\TKBRep.lib %target%\Xbim.ModelGeometry\OpenCascade\Win32\lib /D
+xcopy %src %\win32\vc10\lib\TKMesh.lib %target%\Xbim.ModelGeometry\OpenCascade\Win32\lib /D
+xcopy %src %\win32\vc10\lib\TKPrim.lib %target%\Xbim.ModelGeometry\OpenCascade\Win32\lib /D
+xcopy %src %\win32\vc10\lib\TKBO.lib %target%\Xbim.ModelGeometry\OpenCascade\Win32\lib /D
+xcopy %src %\win32\vc10\lib\TKG3D.lib %target%\Xbim.ModelGeometry\OpenCascade\Win32\lib /D
+xcopy %src %\win32\vc10\lib\TKG2D.lib %target%\Xbim.ModelGeometry\OpenCascade\Win32\lib /D
+xcopy %src %\win32\vc10\lib\TKGeomBase.lib %target%\Xbim.ModelGeometry\OpenCascade\Win32\lib /D
+xcopy %src %\win32\vc10\lib\TKShHealing.lib %target%\Xbim.ModelGeometry\OpenCascade\Win32\lib /D
+
+Echo Win64 Library Release files
+xcopy %src %\Win64\vc10\lib\TKBool.lib %target%\Xbim.ModelGeometry\OpenCascade\Win64\lib /D
+xcopy %src %\Win64\vc10\lib\TKernel.lib %target%\Xbim.ModelGeometry\OpenCascade\Win64\lib /D
+xcopy %src %\Win64\vc10\lib\TKAdvTools.lib %target%\Xbim.ModelGeometry\OpenCascade\Win64\lib /D
+xcopy %src %\Win64\vc10\lib\TKMath.lib %target%\Xbim.ModelGeometry\OpenCascade\Win64\lib /D
+xcopy %src %\Win64\vc10\lib\TKTopAlgo.lib %target%\Xbim.ModelGeometry\OpenCascade\Win64\lib /D
+xcopy %src %\Win64\vc10\lib\TKGeomAlgo.lib %target%\Xbim.ModelGeometry\OpenCascade\Win64\lib /D
+xcopy %src %\Win64\vc10\lib\TKBRep.lib %target%\Xbim.ModelGeometry\OpenCascade\Win64\lib /D
+xcopy %src %\Win64\vc10\lib\TKMesh.lib %target%\Xbim.ModelGeometry\OpenCascade\Win64\lib /D
+xcopy %src %\Win64\vc10\lib\TKPrim.lib %target%\Xbim.ModelGeometry\OpenCascade\Win64\lib /D
+xcopy %src %\Win64\vc10\lib\TKBO.lib %target%\Xbim.ModelGeometry\OpenCascade\Win64\lib /D
+xcopy %src %\Win64\vc10\lib\TKG3D.lib %target%\Xbim.ModelGeometry\OpenCascade\Win64\lib /D
+xcopy %src %\Win64\vc10\lib\TKG2D.lib %target%\Xbim.ModelGeometry\OpenCascade\Win64\lib /D
+xcopy %src %\Win64\vc10\lib\TKGeomBase.lib %target%\Xbim.ModelGeometry\OpenCascade\Win64\lib /D
+xcopy %src %\Win64\vc10\lib\TKShHealing.lib %target%\Xbim.ModelGeometry\OpenCascade\Win64\lib /D
+
+Echo Win32 Binary Release files
+xcopy %src %\win32\vc10\bin\TKBool.dll %target%\Xbim.ModelGeometry\OpenCascade\Win64\bin /D
+xcopy %src %\win32\vc10\bin\TKernel.dll %target%\Xbim.ModelGeometry\OpenCascade\Win64\bin /D
+xcopy %src %\win32\vc10\bin\TKMath.dll %target%\Xbim.ModelGeometry\OpenCascade\Win64\bin /D
+xcopy %src %\win32\vc10\bin\TKTopAlgo.dll %target%\Xbim.ModelGeometry\OpenCascade\Win64\bin /D
+xcopy %src %\win32\vc10\bin\TKGeomAlgo.dll %target%\Xbim.ModelGeometry\OpenCascade\Win64\bin /D
+xcopy %src %\win32\vc10\bin\TKBRep.dll %target%\Xbim.ModelGeometry\OpenCascade\Win64\bin /D
+xcopy %src %\win32\vc10\bin\TKMesh.dll %target%\Xbim.ModelGeometry\OpenCascade\Win64\bin /D
+xcopy %src %\win32\vc10\bin\TKPrim.dll %target%\Xbim.ModelGeometry\OpenCascade\Win64\bin /D
+xcopy %src %\win32\vc10\bin\TKBO.dll %target%\Xbim.ModelGeometry\OpenCascade\Win64\bin /D
+xcopy %src %\win32\vc10\bin\TKG3D.dll %target%\Xbim.ModelGeometry\OpenCascade\Win64\bin /D
+xcopy %src %\win32\vc10\bin\TKG2D.dll %target%\Xbim.ModelGeometry\OpenCascade\Win64\bin /D
+xcopy %src %\win32\vc10\bin\TKGeomBase.dll %target%\Xbim.ModelGeometry\OpenCascade\Win64\bin /D
+xcopy %src %\win32\vc10\bin\TKShHealing.dll %target%\Xbim.ModelGeometry\OpenCascade\Win64\bin /D
+
+Echo Win64 Binary Release files
+xcopy %src %\Win64\vc10\bin\TKBool.dll %target%\Xbim.ModelGeometry\OpenCascade\Win64\bin /D
+xcopy %src %\Win64\vc10\bin\TKernel.dll %target%\Xbim.ModelGeometry\OpenCascade\Win64\bin /D
+xcopy %src %\Win64\vc10\bin\TKMath.dll %target%\Xbim.ModelGeometry\OpenCascade\Win64\bin /D
+xcopy %src %\Win64\vc10\bin\TKTopAlgo.dll %target%\Xbim.ModelGeometry\OpenCascade\Win64\bin /D
+xcopy %src %\Win64\vc10\bin\TKGeomAlgo.dll %target%\Xbim.ModelGeometry\OpenCascade\Win64\bin /D
+xcopy %src %\Win64\vc10\bin\TKBRep.dll %target%\Xbim.ModelGeometry\OpenCascade\Win64\bin /D
+xcopy %src %\Win64\vc10\bin\TKMesh.dll %target%\Xbim.ModelGeometry\OpenCascade\Win64\bin /D
+xcopy %src %\Win64\vc10\bin\TKPrim.dll %target%\Xbim.ModelGeometry\OpenCascade\Win64\bin /D
+xcopy %src %\Win64\vc10\bin\TKBO.dll %target%\Xbim.ModelGeometry\OpenCascade\Win64\bin /D
+xcopy %src %\Win64\vc10\bin\TKG3D.dll %target%\Xbim.ModelGeometry\OpenCascade\Win64\bin /D
+xcopy %src %\Win64\vc10\bin\TKG2D.dll %target%\Xbim.ModelGeometry\OpenCascade\Win64\bin /D
+xcopy %src %\Win64\vc10\bin\TKGeomBase.dll %target%\Xbim.ModelGeometry\OpenCascade\Win64\bin /D
+xcopy %src %\Win64\vc10\bin\TKShHealing.dll %target%\Xbim.ModelGeometry\OpenCascade\Win64\bin /D
