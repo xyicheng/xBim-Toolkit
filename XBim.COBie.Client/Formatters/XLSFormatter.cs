d1e2490071a2aad036b900daff0a4ace6a8a7609
andyward4p_cp <23d1e54b1a95a4b596208405dea5b98c71754f25jLApcpXs>
2012-08-30 10:41:01 +0000

    Added basics of formatting, so Dates fields have correct styling, if the template doesn't have cell styles set. Incomplete for anything other than Date fields.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS13]$/xBIM/XbimFramework/Dev/Akshay;C18239

--- XBim.COBie.Client/Formatters/XLSFormatter.cs
+++ XBim.COBie.Client/Formatters/XLSFormatter.cs
@@ -9,6 +9,8 @@ using Xbim.COBie.Rows;
 using NPOI.SS.Format;
 using System.Globalization;
 using Xbim.COBie.Data;
+using NPOI.SS.UserModel;
+using NPOI.HSSF.Util;
 
 namespace XBim.COBie.Client.Formatters
 {
@@ -21,6 +23,9 @@ namespace XBim.COBie.Client.Formatters
         const string DefaultFileName = "Cobie.xls";
         const string DefaultTemplateFileName = @"Templates\COBie-UK-2012-template.xls";
         const string InstructionsSheet = "Instruction";
+
+        Dictionary<COBieAllowedType, HSSFCellStyle> _cellStyles = new Dictionary<COBieAllowedType, HSSFCellStyle>();
+        Dictionary<string, HSSFColor> _colours = new Dictionary<string, HSSFColor>();
 
         public XLSFormatter() : this(DefaultFileName, DefaultTemplateFileName)
         { }
@@ -37,6 +42,7 @@ namespace XBim.COBie.Client.Formatters
 
         public string FileName { get; set; }
         public string TemplateFileName { get; set; }
+        public HSSFWorkbook Workbook { get; private set; }
 
         /// <summary>
         /// Formats the COBie data into an Excel XLS file
@@ -49,40 +55,95 @@ namespace XBim.COBie.Client.Formatters
             // Load template file
             FileStream excelFile = File.Open(TemplateFileName, FileMode.Open, FileAccess.Read);
 
-            HSSFWorkbook workbook = new HSSFWorkbook(excelFile, true);            
+            Workbook = new HSSFWorkbook(excelFile, true);
+
+            CreateFormats();
 
             // Generically write the sheet
-            WriteSheet<COBieContactRow>(cobie.CobieContacts, workbook);
-            WriteSheet<COBieFacilityRow>(cobie.CobieFacilities, workbook);
-            WriteSheet<COBieFloorRow>(cobie.CobieFloors, workbook);
-            WriteSheet<COBieSpaceRow>(cobie.CobieSpaces, workbook);
-            WriteSheet<COBieZoneRow>(cobie.CobieZones, workbook);
-            WriteSheet<COBieTypeRow>(cobie.CobieTypes, workbook);
-            WriteSheet<COBieComponentRow>(cobie.CobieComponents, workbook);
-            WriteSheet<COBieSystemRow>(cobie.CobieSystems, workbook);
-            WriteSheet<COBieAssemblyRow>(cobie.CobieAssemblies, workbook);
-            WriteSheet<COBieConnectionRow>(cobie.CobieConnections, workbook);
-            WriteSheet<COBieSpareRow>(cobie.CobieSpares, workbook);
-            WriteSheet<COBieResourceRow>(cobie.CobieResources, workbook);
-            WriteSheet<COBieJobRow>(cobie.CobieJobs, workbook);
-            WriteSheet<COBieImpactRow>(cobie.CobieImpacts, workbook);
-            WriteSheet<COBieDocumentRow>(cobie.CobieDocuments, workbook);
-            WriteSheet<COBieAttributeRow>(cobie.CobieAttributes, workbook);
-            WriteSheet<COBieCoordinateRow>(cobie.CobieCoordinates, workbook);
-            WriteSheet<COBieIssueRow>(cobie.CobieIssues, workbook);
-            WriteSheet<COBiePickListsRow>(cobie.CobiePickLists, workbook);
+            WriteSheet<COBieContactRow>(cobie.CobieContacts);
+            WriteSheet<COBieFacilityRow>(cobie.CobieFacilities);
+            WriteSheet<COBieFloorRow>(cobie.CobieFloors);
+            WriteSheet<COBieSpaceRow>(cobie.CobieSpaces);
+            WriteSheet<COBieZoneRow>(cobie.CobieZones);
+            WriteSheet<COBieTypeRow>(cobie.CobieTypes);
+            WriteSheet<COBieComponentRow>(cobie.CobieComponents);
+            WriteSheet<COBieSystemRow>(cobie.CobieSystems);
+            WriteSheet<COBieAssemblyRow>(cobie.CobieAssemblies);
+            WriteSheet<COBieConnectionRow>(cobie.CobieConnections);
+            WriteSheet<COBieSpareRow>(cobie.CobieSpares);
+            WriteSheet<COBieResourceRow>(cobie.CobieResources);
+            WriteSheet<COBieJobRow>(cobie.CobieJobs);
+            WriteSheet<COBieImpactRow>(cobie.CobieImpacts);
+            WriteSheet<COBieDocumentRow>(cobie.CobieDocuments);
+            WriteSheet<COBieAttributeRow>(cobie.CobieAttributes);
+            WriteSheet<COBieCoordinateRow>(cobie.CobieCoordinates);
+            WriteSheet<COBieIssueRow>(cobie.CobieIssues);
+            WriteSheet<COBiePickListsRow>(cobie.CobiePickLists);
 
-            UpdateInstructions(workbook);
+            UpdateInstructions();
 
             using (FileStream exportFile = File.Open(FileName, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None))
             {
-                workbook.Write(exportFile);
+                Workbook.Write(exportFile);
             }
         }
 
-        private void UpdateInstructions(HSSFWorkbook workbook)
+        private void CreateFormats()
         {
-            NPOI.SS.UserModel.ISheet instructionsSheet = workbook.GetSheet(InstructionsSheet);
+            CreateColours();
+            // TODO : Date hardwired to Yellow/Required for now. Only Date is set up for now.
+            CreateFormat(COBieAllowedType.ISODate, "yyyy-MM-ddThh:mm:ss", "Yellow");
+        }
+
+        private void CreateColours()
+        {
+            CreateColours("Yellow", 0xFF, 0xFF, 0x99);
+            CreateColours("Purple", 0xCC, 0x99, 0xFF);
+            CreateColours("Green", 0xCC, 0xFF, 0xCC);
+            CreateColours("Puce", 0xFF, 0xCC, 0x99);
+            CreateColours("Grey", 0x96, 0x96, 0x96);
+        }
+
+        private void CreateColours(string colourName, byte red, byte green, byte blue)
+        {
+            HSSFPalette palette = Workbook.GetCustomPalette();
+            HSSFColor colour = palette.FindSimilarColor(red, green, blue);
+            if (colour == null)
+            {
+                // First 64 are system colours
+                if  (NPOI.HSSF.Record.PaletteRecord.STANDARD_PALETTE_SIZE < 64 )
+                {
+                     NPOI.HSSF.Record.PaletteRecord.STANDARD_PALETTE_SIZE = 64; 
+                }
+                NPOI.HSSF.Record.PaletteRecord.STANDARD_PALETTE_SIZE++;
+                colour = palette.AddColor(red, green, blue);
+            }
+            _colours.Add(colourName, colour);
+        }
+
+        private void CreateFormat(COBieAllowedType type, string formatString, string colourName)
+        {
+            HSSFCellStyle cellStyle;
+            cellStyle = Workbook.CreateCellStyle() as HSSFCellStyle;
+ 
+            HSSFDataFormat dataFormat = Workbook.CreateDataFormat() as HSSFDataFormat;
+            cellStyle.DataFormat = dataFormat.GetFormat(formatString);
+            
+            cellStyle.FillForegroundColor = _colours[colourName].GetIndex();
+            cellStyle.FillPattern = FillPatternType.SOLID_FOREGROUND;
+
+            cellStyle.BorderBottom = BorderStyle.THIN;
+            cellStyle.BorderLeft = BorderStyle.THIN;
+            cellStyle.BorderRight = BorderStyle.THIN;
+            cellStyle.BorderTop = BorderStyle.THIN;
+
+            // TODO:maybe clone from the template?
+            _cellStyles.Add(type, cellStyle);
+        }
+
+        private void UpdateInstructions()
+        {
+            ISheet instructionsSheet = Workbook.GetSheet(InstructionsSheet);
 
             if (instructionsSheet != null)
             {
@@ -96,10 +157,10 @@ namespace XBim.COBie.Client.Formatters
         /// <typeparam name="TCOBieRowType">The type of the row that this COBie sheet contains</typeparam>
         /// <param name="sheet">The COBie sheet which contains the data to be written to the Excel worksheet</param>
         /// <param name="workbook">The Excel workbook</param>
-        private void WriteSheet<TCOBieRowType>(COBieSheet<TCOBieRowType> sheet, HSSFWorkbook workbook) where TCOBieRowType : COBieRow
+        private void WriteSheet<TCOBieRowType>(COBieSheet<TCOBieRowType> sheet) where TCOBieRowType : COBieRow
         {
             
-            NPOI.SS.UserModel.ISheet excelSheet = workbook.GetSheet(sheet.SheetName) ?? workbook.CreateSheet(sheet.SheetName);
+            ISheet excelSheet = Workbook.GetSheet(sheet.SheetName) ?? Workbook.CreateSheet(sheet.SheetName);
 
             // Enumerate rows
             for (int r = 0; r < sheet.Rows.Count; r++)
@@ -107,22 +168,33 @@ namespace XBim.COBie.Client.Formatters
                 TCOBieRowType row = sheet.Rows[r];
 
                 // GET THE ROW + 1 - This stops us overwriting the headers of the worksheet
-                NPOI.SS.UserModel.IRow excelRow = excelSheet.GetRow(r + 1) ?? excelSheet.CreateRow(r + 1);
+                IRow excelRow = excelSheet.GetRow(r + 1) ?? excelSheet.CreateRow(r + 1);
 
                 for (int c = 0; c < sheet.Columns.Count; c++)
                 {
                     COBieCell cell = row[c];
 
-                    NPOI.SS.UserModel.ICell excelCell = excelRow.GetCell(c) ?? excelRow.CreateCell(c);
+                    ICell excelCell = excelRow.GetCell(c) ?? excelRow.CreateCell(c);
                  
                     SetCellValue(excelCell, cell);
+                    FormatCell(excelCell, cell);
                 }
             }
 
             RecalculateSheet(excelSheet);
         }
 
-        private void SetCellValue(NPOI.SS.UserModel.ICell excelCell, COBieCell cell)
+        private void FormatCell(ICell excelCell, COBieCell cell)
+        {
+            HSSFCellStyle style;
+            if (_cellStyles.TryGetValue(cell.CobieCol.AllowedType, out style))
+            {
+                excelCell.CellStyle = style;
+            }
+
+        }
+
+        private void SetCellValue(ICell excelCell, COBieCell cell)
         {
             try
             {
@@ -155,7 +227,7 @@ namespace XBim.COBie.Client.Formatters
         }
 
         
-        private static void RecalculateSheet(NPOI.SS.UserModel.ISheet excelSheet)
+        private static void RecalculateSheet(ISheet excelSheet)
         {
             // Ensures the spreadsheet formulas will be recalulated the next time the file is opened
             excelSheet.ForceFormulaRecalculation = true;
