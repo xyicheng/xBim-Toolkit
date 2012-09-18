27a79eb09b707db65773cdd0739a71d7000aa79a
Skoropinski_cp <1c0ac7afad3e4c11c8ef29dee019849da45e7e6e36QBJDEJ>
2012-09-18 09:43:01 +0000

    Added data mapping code for COBie System, Spare, Resources and Job sheets, fixed property in IfcRelSequence, moved Assembly Type row in COBieAssemblyRow.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS13]$/xBIM/XbimFramework/Dev/4Projects;C18742

---
+++ Xbim.Tests/XBimGeneralTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+using Xbim.XbimExtensions;
+using Xbim.IO;
+using Xbim.Ifc.Extensions;
+using Xbim.XbimExtensions.Parser;
+using Xbim.Ifc.Kernel;
+using Xbim.Ifc.SelectTypes;
+using Xbim.XbimExtensions.Transactions.Extensions;
+using System.Diagnostics;
+namespace Xbim.Tests
+{
+    [TestClass]
+    public class XBimGeneralTest
+    {
+        [TestMethod]
+        public void Test_Instances()
+        {
+            Module ifcModule = typeof(IfcActor).Module;
+            IEnumerable<Type> types =
+                ifcModule.GetTypes().Where(
+                    t =>
+                    typeof(IPersistIfc).IsAssignableFrom(t) && t != typeof(IPersistIfc) && !t.IsEnum && t.IsAbstract &&
+                    t.IsPublic && !typeof(ExpressHeaderType).IsAssignableFrom(t));
+            foreach (Type ifctype in types)
+            {
+                //Console.WriteLine(ifctype.Name);
+                Trace.WriteLine(ifctype.Name);
+                //Debug.WriteLine(ifctype.Name);
+            }
+        }
+    }
+}
