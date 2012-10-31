65039b4c19baa1aaf43b6a8bb05177a482ef5b42
andyward4p_cp <23d1e54b1a95a4b596208405dea5b98c71754f25jLApcpXs>
2012-10-31 20:46:17 +0000

    No functional change. Added the CDDL licence and 3rd party licences, plus a Readme.txt file referring to the components and their respective licences (as I understand them to be) Fixed licence headers on a number of 3rd party source files which appear to have been incorrectly attributed to XBIM.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS13]$/xBIM/XbimFramework/Dev/COBie;C20043

--- Xbim.Ifc/XbimExtensions/Parser/ShiftReduceParserCode.cs
+++ Xbim.Ifc/XbimExtensions/Parser/ShiftReduceParserCode.cs
@@ -1,14 +1,6 @@
-#region XbimHeader
-
-// The eXtensible Building Information Modelling (xBIM) Toolkit
-// Solution:    XbimComplete
-// Project:     Xbim.Ifc
-// Filename:    ShiftReduceParserCode.cs
-// Published:   01, 2012
-// Last Edited: 9:04 AM on 20 12 2011
-// (See accompanying copyright.rtf)
-
-#endregion
+// Gardens Point Parser Generator
+// Copyright (c) Wayne Kelly, QUT 2005-2010
+// (see accompanying NewBSD Licence)
 
 #define EXPORT_GPPG
 
