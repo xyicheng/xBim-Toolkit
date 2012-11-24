556610ba7f1427c5cbc4c77b8ccad20f7659c98f
SteveLockley_cp <9623b022d0bb1e357feec713e5d0abd194558924hm7A5WAR>
2012-11-24 09:24:27 +0000

    Indexing of IfcProperty added
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS13]$/xBIM/XbimFramework/Dev/Steve;C21138

--- Xbim.Ifc2x3/PropertyResource/IfcProperty.cs
+++ Xbim.Ifc2x3/PropertyResource/IfcProperty.cs
@@ -56,7 +56,7 @@ namespace Xbim.Ifc2x3.PropertyResource
     /// <summary>
     ///   Definition from IAI: An abstract generalization for all types of properties that can be associated with IFC objects through the property set mechanism.
     /// </summary>
-    [IfcPersistedEntityAttribute, Serializable]
+    [IfcPersistedEntityAttribute, Serializable, IndexedClass]
     public abstract class IfcProperty : INotifyPropertyChanged, ISupportChangeNotification, IPersistIfcEntity,
                                         INotifyPropertyChanging
     {
