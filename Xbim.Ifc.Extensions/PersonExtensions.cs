9df11009c8f249c4703d6c828169702172eab1dd
Skoropinski_cp <1c0ac7afad3e4c11c8ef29dee019849da45e7e6e36QBJDEJ>
2013-03-22 12:22:56 +0000

    Added Extension methods for creating strings from property values and units, fix on x64 convert output path
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS13]$/xBIM/XbimFramework/Dev/COBie;C25091

---
+++ Xbim.Ifc.Extensions/PersonExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xbim.Ifc2x3.ActorResource;
+
+namespace Xbim.Ifc2x3.Extensions
+{
+    public static class PersonExtensions
+    {
+        public static string GetFullName(this IfcPerson ifcPerson)
+        {
+            string name = string.Empty;
+            foreach (var item in ifcPerson.PrefixTitles)
+            {
+                name += string.IsNullOrEmpty(item) ? "" : item.ToString() + " ";
+            }
+
+            if (ifcPerson.GivenName.HasValue)
+                name += ifcPerson.GivenName + " ";
+
+            foreach (var item in ifcPerson.MiddleNames)
+            {
+                name += string.IsNullOrEmpty(item) ? "" : item.ToString() + " ";
+            }
+
+            if (ifcPerson.FamilyName.HasValue)
+                name += ifcPerson.FamilyName + " ";
+
+            foreach (var item in ifcPerson.SuffixTitles)
+            {
+                name += string.IsNullOrEmpty(item) ? "" : item.ToString() + " ";
+            }
+            return name;
+        }
+    }
+}
