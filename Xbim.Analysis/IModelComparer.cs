263ae80b829d9a765d090e5ccafb7ff3d7aa49dd
LloydPickering_cp <085be1d632ba650d7993645ec428955ae63d4a6aAnXbJNcC>
2013-07-16 13:32:52 +0000

    Added in the start of some differencing capabilities
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS13]$/xBIM/XbimFramework/Head;C27907

---
+++ Xbim.Analysis/IModelComparer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xbim.Ifc2x3.Kernel;
+
+namespace Xbim.Analysis
+{
+    public interface IModelComparer
+    {
+        Dictionary<IfcProduct, ChangeType> Compare(IEnumerable<IfcProduct> Baseline, IEnumerable<IfcProduct> Delta);
+        Dictionary<Int32, Int32> GetMap();
+    }
+    public enum ChangeType { 
+        Added,
+        Deleted,
+        Modified,
+        Matched,
+        Unknown
+    }
+}
