4e155a527affda2b6d5eaed1b26916d8ad09f7bf
Bonghi_cp <ec53983f5533846d33b7dfcca9568baa012d78bc4EVBPK2p>
2013-09-07 22:33:35 +0000

    Simple comments added to the DynamicGrouping functions and classes In Xbim.IO. Test cases added also demonstrate the use of the feature.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS13]$/xBIM/XbimFramework/Head;C28896

---
+++ Xbim.Tests/XmlDynamicGroupingTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xbim.IO;
+using Xbim.IO.DynamicGrouping;
+using Xbim.XbimExtensions.Interfaces;
+
+namespace Xbim.Tests
+{
+    [TestClass]
+    // [DeploymentItem(TestSourceFileIfc, Root)]
+    [DeploymentItem(TestXbimFile, ModelFolder)]
+    [DeploymentItem(XmlNRM_ClassFile, XmlFolder)]
+    [DeploymentItem(XmlNRM_RulesFile, XmlFolder)]
+    public class XmlDynamicGroupingTests
+    {
+        const string ModelFolder = "TestSourceFiles";
+        const string TestXbimFile = ModelFolder + "/Duplex.xbim";
+
+        const string XmlFolder = "DynamicGrouping";
+        const string XmlNRM_ClassFile = XmlFolder + "/NRM clssification.xml";
+        const string XmlNRM_RulesFile = XmlFolder + "/NRM2IFC.xml";
+
+        [TestMethod]
+        public void GroupNRM()
+        {
+            Debug.WriteLine(Directory.GetCurrentDirectory());
+
+            XbimModel model = new XbimModel();
+            model.Open(TestXbimFile, XbimExtensions.XbimDBAccess.ReadWrite);
+            
+            GroupsFromXml g = new GroupsFromXml(model);
+            g.CreateGroups(XmlNRM_ClassFile);
+            
+            GroupingByXml grouping = new GroupingByXml(model);
+            grouping.GroupElements(XmlNRM_RulesFile);
+  
+            model.Close();
+        }
+    }
+}
