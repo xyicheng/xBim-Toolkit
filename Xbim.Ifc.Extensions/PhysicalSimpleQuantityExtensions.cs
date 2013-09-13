d7dfc4696cf466b13ed2f5a115b2688151ddb21e
Bonghi_cp <ec53983f5533846d33b7dfcca9568baa012d78bc4EVBPK2p>
2013-09-13 11:39:47 +0000

    Extension classes for Unit management improved. Querying interface presents more information on schema classes when called with "full" option.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS13]$/xBIM/XbimFramework/Head;C28950

---
+++ Xbim.Ifc.Extensions/PhysicalSimpleQuantityExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xbim.Ifc2x3.MeasureResource;
+using Xbim.Ifc2x3.QuantityResource;
+
+namespace Xbim.Ifc2x3.Extensions
+{
+    public static class PhysicalSimpleQuantityExtensions
+    {
+        /// <summary>
+        /// Returns the named unit for the specified quantity.
+        /// 
+        /// EFFICIENCY WARNING: *****************
+        /// 
+        /// this requires a number of Database calls. In case you have to run queries on multiple IfcPhysicalSimpleQuantity it's far better 
+        /// to get the instance of IfcUnitAssignment from the model and use its GetUnitFor() method.
+        /// </summary>
+        /// <returns></returns>
+        public static IfcNamedUnit GetResolvedUnit(this IfcPhysicalSimpleQuantity ifcPhysicalSimpleQuantity)
+        {
+            if (ifcPhysicalSimpleQuantity.Unit != null)
+                return ifcPhysicalSimpleQuantity.Unit;
+            IfcUnitAssignment modelUnits = ifcPhysicalSimpleQuantity.ModelOf.Instances.OfType<IfcUnitAssignment>().FirstOrDefault(); // not optional, should never return void in valid model
+            return modelUnits.GetUnitFor(ifcPhysicalSimpleQuantity);
+        }
+    }
+}
