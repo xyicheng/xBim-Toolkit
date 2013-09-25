35f540864c2e385184cfc60d0d4e64b866796fbb
martin1cerny_cp <180bca91876c2e0fc4dabfaf73519922dc5ab3dfCvEqhAtg>
2013-09-25 14:03:17 +0000

    Minor changes and fixes so that Xbim.Spatial compiles.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS13]$/xBIM/XbimFramework/Dev/Martin;C29169

--- Xbim.Spatial/Extensions/ModelExtensions.cs
+++ Xbim.Spatial/Extensions/ModelExtensions.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.IO;
 using Xbim.Ifc2x3.Kernel;
 using Xbim.XbimExtensions.Interfaces;
-using Xbim.Shapefile;
+//using Xbim.Shapefile;
 using Xbim.ModelGeometry;
 using Xbim.Ifc2x3.RepresentationResource;
 using Xbim.ModelGeometry.Scene;
@@ -21,48 +21,51 @@ namespace Xbim.Spatial.Extensions
         /// </summary>
         public static void SaveAsShapefile(this IModel model, string directory)
         {
-            if (!Directory.Exists(directory))
-                Directory.CreateDirectory(directory);
-            IEnumerable<IfcProduct> prods = model.Instances.OfType<IfcProduct>();
-            IEnumerable<IGrouping<Type, IfcProduct>> groups = prods.GroupBy<IfcProduct, Type>( t => t.GetType());
+            //if (!Directory.Exists(directory))
+            //    Directory.CreateDirectory(directory);
+            //IEnumerable<IfcProduct> prods = model.Instances.OfType<IfcProduct>();
+            //IEnumerable<IGrouping<Type, IfcProduct>> groups = prods.GroupBy<IfcProduct, Type>( t => t.GetType());
 
-            //create a map dictionary to hold represenations that are repeated maps
-            Dictionary<IfcRepresentation, IXbimGeometryModel> maps = new Dictionary<IfcRepresentation, IXbimGeometryModel>();
+            ////create a map dictionary to hold represenations that are repeated maps
+            //Dictionary<IfcRepresentation, IXbimGeometryModel> maps = new Dictionary<IfcRepresentation, IXbimGeometryModel>();
 
-            foreach (var group in groups)
-            {
-                //this will be used as a name of the shapefile
-                string name = group.Key.Name;
+            //foreach (var group in groups)
+            //{
+            //    //this will be used as a name of the shapefile
+            //    string name = group.Key.Name;
 
-                //Creation of SHP and DBF file
-                IShapefileSHPWriter handleSHP = new SHPObjectData(name, ShapeType.MultiPatch);
-                IShapefileDBFWriter handleDBF = new DBFObjectData(name);
-
-
-                foreach (var item in group)
-                {
-                    //write attributes
-                    handleDBF.addValue(item.EntityLabel, DBFFieldType.FTInteger, "Label", 1, 0);
-                    handleDBF.addValue(item.GlobalId.ToString(), DBFFieldType.FTString, "Guid", 30, 0);
-                    handleDBF.addValue(item.Name.ToString(), DBFFieldType.FTString, "Name", 30, 0);
-                    handleDBF.addValue(item.EntityLabel, DBFFieldType.FTInteger, "Label", 1, 0);
-                    handleDBF.writeRow();
+            //    //Creation of SHP and DBF file
+            //    IShapefileSHPWriter handleSHP = new SHPObjectData(name, ShapeType.MultiPatch);
+            //    IShapefileDBFWriter handleDBF = new DBFObjectData(name);
 
 
-                    //get geometry
-                    //Get an interface to the XbimGeometry
-                    IXbimGeometryModel iGeom = XbimGeometryModel.CreateFrom(item, true, XbimLOD.LOD_Unspecified, false);
+            //    foreach (var item in group)
+            //    {
+            //        //write attributes
+            //        handleDBF.addValue(item.EntityLabel, DBFFieldType.FTInteger, "Label", 1, 0);
+            //        handleDBF.addValue(item.GlobalId.ToString(), DBFFieldType.FTString, "Guid", 30, 0);
+            //        handleDBF.addValue(item.Name.ToString(), DBFFieldType.FTString, "Name", 30, 0);
+            //        handleDBF.addValue(item.EntityLabel, DBFFieldType.FTInteger, "Label", 1, 0);
+            //        handleDBF.writeRow();
 
-                    List<XbimTriangulatedModel> triangModels = iGeom.Mesh();
-                    foreach (var triangModel in triangModels)
-                    {
-                        //write geometry to the SHP
-                    }
+
+            //        //get geometry
+            //        //Get an interface to the XbimGeometry
+            //        IXbimGeometryModel iGeom = XbimGeometryModel.CreateFrom(item, true, XbimLOD.LOD_Unspecified, false);
+
+            //        List<XbimTriangulatedModel> triangModels = iGeom.Mesh();
+            //        foreach (var triangModel in triangModels)
+            //        {
+            //            //write geometry to the SHP
+            //            throw new NotImplementedException();
+            //        }
                     
-                    handleSHP.writeData();
-                }
+            //        handleSHP.writeData();
+            //    }
 
-            }
+            //}
+
+            throw new NotImplementedException();
         }
     }
 }
