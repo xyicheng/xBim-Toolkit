509f54c096fc8cf427a944ad393e135b614e9381
martin1cerny_cp <180bca91876c2e0fc4dabfaf73519922dc5ab3dfCvEqhAtg>
2013-09-25 10:56:21 +0000

    Xbim.Script converted to be a library. XbimInterpret is now one of samples like XbimXplorer and it is a command line interpret of the script. Xbim.IO setting fixed so that everything compiles and works now.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS13]$/xBIM/XbimFramework/Dev/Martin;C29160

---
+++ XbimInterpret/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+using System.IO;
+using Xbim.Script;
+using Xbim.IO;
+
+namespace XbimInterpret
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testModelPath = @"G:\CODE\xBIM\Martin\XbimQueryTest\TestModels\Standard Classroom.ifc";
+            var isTestModel = File.Exists(testModelPath);
+
+            XbimModel model = XbimModel.CreateTemporaryModel();
+            XbimQueryParser parser = new XbimQueryParser(model);
+
+            if (args.Count() > 0)
+            {
+                Stream reader = File.Open(args[0], FileMode.Open, FileAccess.Read);
+                parser.Parse(reader);
+
+                if (parser.Errors.Count() == 0)
+                    Console.WriteLine("Input file processed");
+                else
+                {
+                    Console.WriteLine("Input file processed with errors");
+                    FileStream errFile = File.Open("Errors.txt", FileMode.OpenOrCreate, FileAccess.Write);
+                    var errLog = new StreamWriter(errFile);
+                    foreach (var err in parser.Errors)
+                    {
+                        Console.WriteLine(err);
+                        errLog.WriteLine(err);
+                    }
+                    errLog.Close();
+                }
+
+            }
+            
+
+            Console.WriteLine("Enter your commands (use 'exit' command to quit):");
+            Console.Write(">>> ");
+            string command = Console.ReadLine();
+            
+            while (command.ToLower() != "exit")
+            {
+                if (command == "X" && isTestModel)
+                {
+                    Console.WriteLine("Opening model...");
+                    model.CreateFrom(testModelPath, null, null, true);
+                    Console.WriteLine("Model ready...");
+                }
+                parser.Parse(command);
+                if (parser.Errors.Count() != 0)
+                    foreach (var error in parser.Errors)
+                    {
+                        Console.WriteLine(error);
+                    }
+
+                Console.Write(">>> ");
+                command = Console.ReadLine();
+            }
+
+            model.Close();
+        }
+    }
+}
