3d71f705e148adc341b9bf1011b787589fd287a2
Skoropinski_cp <1c0ac7afad3e4c11c8ef29dee019849da45e7e6e36QBJDEJ>
2013-10-10 12:51:44 +0000

    Fix on "Pset_Warranty" changed to "COBie_Warranty" and "Pset_Specification" changed to "COBie_Specification"
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS13]$/xBIM/XbimFramework/Rel/RC5;C29665

--- Xbim.COBie/Serialisers/XbimSerialiser/COBieXBimType.cs
+++ Xbim.COBie/Serialisers/XbimSerialiser/COBieXBimType.cs
@@ -142,17 +142,18 @@ namespace Xbim.COBie.Serialisers.XbimSerialiser
                     if (ValidateString(row.ModelNumber))
                         AddPropertySingleValue(ifcTypeObject, "Pset_ManufacturersTypeInformation", null, "ModelLabel", "Model Number for " + name, new IfcLabel(row.ModelNumber));
                     if (ValidateString(row.WarrantyGuarantorParts))
-                        AddPropertySingleValue(ifcTypeObject, "Pset_Warranty", "Warranty Information", "WarrantyGuarantorParts", "Warranty Contact for " + name, new IfcLabel(row.WarrantyGuarantorParts));
+                        //reset property set name from "Pset_Warranty" to "COBie_Warranty"
+                        AddPropertySingleValue(ifcTypeObject, "COBie_Warranty", "Warranty Information", "WarrantyGuarantorParts", "Warranty Contact for " + name, new IfcLabel(row.WarrantyGuarantorParts));
                     if (ValidateString(row.WarrantyDurationParts))
-                        AddPropertySingleValue(ifcTypeObject, "Pset_Warranty", null, "WarrantyDurationParts", "Warranty length for " + name, new IfcLabel(row.WarrantyDurationParts));
+                        AddPropertySingleValue(ifcTypeObject, "COBie_Warranty", null, "WarrantyDurationParts", "Warranty length for " + name, new IfcLabel(row.WarrantyDurationParts));
                     if (ValidateString(row.WarrantyGuarantorLabor))
-                        AddPropertySingleValue(ifcTypeObject, "Pset_Warranty", null, "WarrantyGuarantorLabor", "Warranty Labour Contact for " + name, new IfcLabel(row.WarrantyGuarantorLabor));
+                        AddPropertySingleValue(ifcTypeObject, "COBie_Warranty", null, "WarrantyGuarantorLabor", "Warranty Labour Contact for " + name, new IfcLabel(row.WarrantyGuarantorLabor));
                     if (ValidateString(row.WarrantyDescription))
-                        AddPropertySingleValue(ifcTypeObject, "Pset_Warranty", null, "WarrantyDescription", "Warranty Description for" + name, new IfcLabel(row.WarrantyDescription));
+                        AddPropertySingleValue(ifcTypeObject, "COBie_Warranty", null, "WarrantyDescription", "Warranty Description for" + name, new IfcLabel(row.WarrantyDescription));
 
                     if (ValidateString(row.WarrantyDurationLabor))
                     {
-                        IfcPropertySingleValue ifcPropertySingleValue = AddPropertySingleValue(ifcTypeObject, "Pset_Warranty", null, "WarrantyDurationLabor", "Labour Warranty length for " + name, new IfcLabel(row.WarrantyDurationLabor));
+                        IfcPropertySingleValue ifcPropertySingleValue = AddPropertySingleValue(ifcTypeObject, "COBie_Warranty", null, "WarrantyDurationLabor", "Labour Warranty length for " + name, new IfcLabel(row.WarrantyDurationLabor));
                         //WarrantyDurationUnit
                         if (ValidateString(row.WarrantyDurationUnit))
                             ifcPropertySingleValue.Unit = GetDurationUnit(row.WarrantyDurationUnit);
@@ -174,57 +175,58 @@ namespace Xbim.COBie.Serialisers.XbimSerialiser
                     if (ValidateString(row.NominalLength))
                     {
                         double? value = GetDoubleFromString(row.NominalLength);
+                        //changed from "Pset_Specification" to "COBie_Specification" via v16 matrix sheet
                         if (value != null)
-                            AddPropertySingleValue(ifcTypeObject, "Pset_Specification", "Specification Properties", "NominalLength", "Nominal Length Value for " + name, new IfcReal((double)value));
+                            AddPropertySingleValue(ifcTypeObject, "COBie_Specification", "Specification Properties", "NominalLength", "Nominal Length Value for " + name, new IfcReal((double)value));
                     }
                     if (ValidateString(row.NominalWidth))
                     {
                         double? value = GetDoubleFromString(row.NominalWidth);
                         if (value != null)
-                            AddPropertySingleValue(ifcTypeObject, "Pset_Specification", null, "NominalWidth", "Nominal Width Value for " + name, new IfcReal((double)value));
+                            AddPropertySingleValue(ifcTypeObject, "COBie_Specification", null, "NominalWidth", "Nominal Width Value for " + name, new IfcReal((double)value));
                     }
                     if (ValidateString(row.NominalHeight))
                     {
                         double? value = GetDoubleFromString(row.NominalHeight);
                         if (value != null)
-                            AddPropertySingleValue(ifcTypeObject, "Pset_Specification", null, "NominalHeight", "Nominal Height Value for " + name, new IfcReal((double)value));
+                            AddPropertySingleValue(ifcTypeObject, "COBie_Specification", null, "NominalHeight", "Nominal Height Value for " + name, new IfcReal((double)value));
                     }
 
                     if (ValidateString(row.ModelReference))
-                        AddPropertySingleValue(ifcTypeObject, "Pset_Specification", null, "ModelReference", "Model Reference Value for " + name, new IfcLabel(row.ModelReference));
+                        AddPropertySingleValue(ifcTypeObject, "COBie_Specification", null, "ModelReference", "Model Reference Value for " + name, new IfcLabel(row.ModelReference));
 
                     if (ValidateString(row.Shape))
-                        AddPropertySingleValue(ifcTypeObject, "Pset_Specification", null, "Shape", "Shape Value for " + name, new IfcLabel(row.Shape));
+                        AddPropertySingleValue(ifcTypeObject, "COBie_Specification", null, "Shape", "Shape Value for " + name, new IfcLabel(row.Shape));
 
                     if (ValidateString(row.Size))
-                        AddPropertySingleValue(ifcTypeObject, "Pset_Specification", null, "Size", "Size Value for " + name, new IfcLabel(row.Size));
+                        AddPropertySingleValue(ifcTypeObject, "COBie_Specification", null, "Size", "Size Value for " + name, new IfcLabel(row.Size));
 
                     if (ValidateString(row.Color))
-                        AddPropertySingleValue(ifcTypeObject, "Pset_Specification", null, "Color", "Color Value for " + name, new IfcLabel(row.Color));
+                        AddPropertySingleValue(ifcTypeObject, "COBie_Specification", null, "Color", "Color Value for " + name, new IfcLabel(row.Color));
 
                     if (ValidateString(row.Finish))
-                        AddPropertySingleValue(ifcTypeObject, "Pset_Specification", null, "Finish", "Finish Value for " + name, new IfcLabel(row.Finish));
+                        AddPropertySingleValue(ifcTypeObject, "COBie_Specification", null, "Finish", "Finish Value for " + name, new IfcLabel(row.Finish));
 
                     if (ValidateString(row.Grade))
-                        AddPropertySingleValue(ifcTypeObject, "Pset_Specification", null, "Grade", "Grade Value for " + name, new IfcLabel(row.Grade));
+                        AddPropertySingleValue(ifcTypeObject, "COBie_Specification", null, "Grade", "Grade Value for " + name, new IfcLabel(row.Grade));
 
                     if (ValidateString(row.Material))
-                        AddPropertySingleValue(ifcTypeObject, "Pset_Specification", null, "Material", "Material Value for " + name, new IfcLabel(row.Material));
+                        AddPropertySingleValue(ifcTypeObject, "COBie_Specification", null, "Material", "Material Value for " + name, new IfcLabel(row.Material));
 
                     if (ValidateString(row.Constituents))
-                        AddPropertySingleValue(ifcTypeObject, "Pset_Specification", null, "Constituents", "Constituents Value for " + name, new IfcLabel(row.Constituents));
+                        AddPropertySingleValue(ifcTypeObject, "COBie_Specification", null, "Constituents", "Constituents Value for " + name, new IfcLabel(row.Constituents));
 
                     if (ValidateString(row.Features))
-                        AddPropertySingleValue(ifcTypeObject, "Pset_Specification", null, "Features", "Features Value for " + name, new IfcLabel(row.Features));
+                        AddPropertySingleValue(ifcTypeObject, "COBie_Specification", null, "Features", "Features Value for " + name, new IfcLabel(row.Features));
 
                     if (ValidateString(row.AccessibilityPerformance))
-                        AddPropertySingleValue(ifcTypeObject, "Pset_Specification", null, "AccessibilityPerformance", "Accessibility Performance Value for " + name, new IfcLabel(row.AccessibilityPerformance));
+                        AddPropertySingleValue(ifcTypeObject, "COBie_Specification", null, "AccessibilityPerformance", "Accessibility Performance Value for " + name, new IfcLabel(row.AccessibilityPerformance));
 
                     if (ValidateString(row.CodePerformance))
-                        AddPropertySingleValue(ifcTypeObject, "Pset_Specification", null, "CodePerformance", "Code Performance Value for " + name, new IfcLabel(row.CodePerformance));
+                        AddPropertySingleValue(ifcTypeObject, "COBie_Specification", null, "CodePerformance", "Code Performance Value for " + name, new IfcLabel(row.CodePerformance));
 
                     if (ValidateString(row.SustainabilityPerformance))
-                        AddPropertySingleValue(ifcTypeObject, "Pset_Specification", null, "SustainabilityPerformance", "Sustainability Performance Value for " + name, new IfcLabel(row.SustainabilityPerformance));
+                        AddPropertySingleValue(ifcTypeObject, "COBie_Specification", null, "SustainabilityPerformance", "Sustainability Performance Value for " + name, new IfcLabel(row.SustainabilityPerformance));
 
 
                 }
