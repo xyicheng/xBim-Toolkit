45717046d6ac38dbe7d184d816cf913960293f34
martin1cerny_cp <180bca91876c2e0fc4dabfaf73519922dc5ab3dfCvEqhAtg>
2014-01-25 19:57:09 +0000

    Comparison Manager test implemented.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS13]$/xBIM/XbimFramework/Dev/ModelServer;C32534

---
+++ Xbim.Tests/Comparisons/ComparisonManagerTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xbim.IO;
+using Xbim.Ifc2x3.Kernel;
+using Xbim.Analysis.Comparing;
+
+namespace Xbim.Tests.Comparisons
+{
+    [TestClass]
+    public class ComparisonManagerTests
+    {
+        [TestMethod]
+        public void ComparisonManagerTest()
+        {
+            var baseline = new XbimModel();
+            baseline.CreateFrom("OneRoom.ifc", "ComparisonManagerTestBaseline", null, true, false);
+            var revision = new XbimModel();
+            revision.CreateFrom("OneRoom_added_window.ifc", "ComparisonManagerTestRevision", null, true, false);
+
+
+            var comparerAttribute = new AttributeComparer("ObjectType", revision);
+            var comparerGeom = new GeometryComparerII(baseline, revision);
+            var comparerGuid = new GuidComparer();
+            var comparerMaterial = new MaterialComparer(revision);
+            var comparerName = new NameComparer();
+            var comparerProperty = new PropertyComparer(baseline, revision);
+
+            var manager = new ComparisonManager(baseline, revision);
+            manager.AddComparer(comparerAttribute);
+            manager.AddComparer(comparerGeom);
+            manager.AddComparer(comparerGuid);
+            manager.AddComparer(comparerMaterial);
+            manager.AddComparer(comparerName);
+            manager.AddComparer(comparerProperty);
+
+
+            manager.Compare<IfcProduct>();
+            var results = manager.Results.Results.ToList();
+        }
+    }
+}
