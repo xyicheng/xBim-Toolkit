9de60f07cf3644bc484921abed3b2e8ae027701e
Bonghi_cp <ec53983f5533846d33b7dfcca9568baa012d78bc4EVBPK2p>
2014-02-28 16:46:18 +0000

    Implementation of IfcSharedMgmtElements namespace implemented in Ifc2x3. Untested as no application exports any of these classes as far as we know.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS13]$/xBIM/XbimFramework/Head;C33046

---
+++ Xbim.Ifc2x3/SharedMgmtElements/IfcRelAssignsToProjectOrder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xbim.Ifc2x3.Kernel;
+
+namespace Xbim.Ifc2x3.SharedMgmtElements
+{
+    public class IfcRelAssignsToProjectOrder : IfcRelAssignsToControl
+    {
+    }
+}
