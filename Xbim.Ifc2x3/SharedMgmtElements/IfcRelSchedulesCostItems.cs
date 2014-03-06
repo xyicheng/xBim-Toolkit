6157b7ae7ce1de57061679acbb76f417f72aab4e
Bonghi_cp <ec53983f5533846d33b7dfcca9568baa012d78bc4EVBPK2p>
2014-03-06 16:43:41 +0000

    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS13]$/xBIM/XbimFramework/Head;C33126

---
+++ Xbim.Ifc2x3/SharedMgmtElements/IfcRelSchedulesCostItems.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xbim.Ifc2x3.Kernel;
+
+namespace Xbim.Ifc2x3.SharedMgmtElements
+{
+    /// <summary>
+    /// An IfcRelSchedulesCostItems is a subtype of IfcRelAssignsToControl that enables one or many instances of IfcCostItem to be assigned to an instance of IfcCostSchedule.
+    /// HISTORY: New Entity in IFC Release 2.0. Modified in IFC 2x2
+    /// </summary>
+    class IfcRelSchedulesCostItems : IfcRelAssignsToControl
+    {
+        // todo: implement where()
+    }
+}
