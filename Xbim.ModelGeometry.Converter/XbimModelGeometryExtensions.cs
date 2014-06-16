2453752752f7d0f8f7eed1306b6a673a24d8a53e
SteveLockley_cp <9623b022d0bb1e357feec713e5d0abd194558924hm7A5WAR>
2014-06-16 07:40:11 +0000

    Geometry Extension methods added to Xvim.ModelGeometry.Convert
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS13]$/xBIM/XbimFramework/Dev/ModelServer;C34573

--- Xbim.ModelGeometry.Converter/XbimModelGeometryExtensions.cs
+++ Xbim.ModelGeometry.Converter/XbimModelGeometryExtensions.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using Xbim.Common.Logging;
+using Xbim.Ifc2x3.GeometricModelResource;
+using Xbim.Ifc2x3.GeometryResource;
+using Xbim.Ifc2x3.Kernel;
+using Xbim.Ifc2x3.RepresentationResource;
 using Xbim.IO;
 using Xbim.ModelGeometry.Scene;
 using Xbim.XbimExtensions;
@@ -17,5 +21,42 @@ namespace Xbim.ModelGeometry.Converter
              IXbimGeometryEngine engine = ((IModel)model).GeometryEngine();
              return engine.GetGeometry3D(plyData, geomType);
          }
+
+         public static IXbimGeometryModelGroup Geometry3D(this IfcRepresentationItem repItem)
+         {
+             IXbimGeometryEngine engine = repItem.ModelOf.GeometryEngine();
+             if (engine != null) return engine.GetGeometry3D(repItem);
+             else return XbimEmptyGeometryGroup.Empty;
+         }
+
+         public static IXbimGeometryModelGroup Geometry3D(this IfcProduct product, XbimGeometryType xbimGeometryType)
+         {
+             IXbimGeometryEngine engine = product.ModelOf.GeometryEngine();
+             if (engine != null) return engine.GetGeometry3D(product, xbimGeometryType);
+             else return XbimEmptyGeometryGroup.Empty;
+         }
+
+         public static IXbimGeometryModelGroup Geometry3D(this IfcProduct product)
+         {
+             IXbimGeometryEngine engine = product.ModelOf.GeometryEngine();
+             if (engine != null) return engine.GetGeometry3D(product);
+             else return XbimEmptyGeometryGroup.Empty;
+         }
+
+         public static IXbimGeometryModelGroup Geometry3D(this IfcRepresentation representation)
+         {
+             IXbimGeometryEngine engine = representation.ModelOf.GeometryEngine();
+             if (engine != null) return engine.GetGeometry3D(representation);
+             else return XbimEmptyGeometryGroup.Empty;
+         }
+
+         public static IXbimGeometryModelGroup Geometry3D(this IfcSolidModel solid, XbimGeometryType xbimGeometryType)
+         {
+             IXbimGeometryEngine engine = solid.ModelOf.GeometryEngine();
+             if (engine != null) return engine.GetGeometry3D(solid, xbimGeometryType);
+             else return XbimEmptyGeometryGroup.Empty;
+         }
+
+
     }
 }
